78e89e683b0cb2ff014e1e74a65fd9be38314cc6
Jan-Kristian Herring <jankristianherring@gmail.com>
2024-04-13 23:43:25 +0300

    Final results as rank

--- LDJAM55/Assets/Scripts/Backend/ProductState.cs
+++ LDJAM55/Assets/Scripts/Backend/ProductState.cs
@@ -1,22 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 namespace DataTypes
 {
     // Final results for game end screen
     public struct FinalResult
     {
-        public float funScore;
-        public float innovationScore;
-        public float themeScore;
-        public float graphicsScore;
-        public float audioScore;
-        public float humorScore;
-        public float moodScore;
-        public float overallScore;
+        public int funRank;
+        public int innovationRank;
+        public int themeRank;
+        public int graphicsRank;
+        public int audioRank;
+        public int humorRank;
+        public int moodRank;
+        public int overallRank;
     }
 
     public class ProductState
     {
+        const int otherContestantCount = 999;
         const float scoreLowerBound = 0f;
         const float scoreUpperBound = 1f;
 
@@ -60,16 +62,56 @@ namespace DataTypes
 
             float polishPenalty = 0.25f + 0.75f * polishFeature;
 
-            result.funScore = ((funScore * 4f + mechanicsFeature * 2f + audioFeature + visualsFeature) / 8f) * polishPenalty;
-            result.innovationScore = (innovationScore * 3f + mechanicsFeature + audioFeature * 0.5f + visualsFeature * 0.5f) / 5f;
-            result.themeScore = (themeScore * 3f + mechanicsFeature * 0.5f + audioFeature * 0.75f + visualsFeature * 0.75f) / 5f;
-            result.graphicsScore = ((graphicsScore + visualsFeature) / 2f) * polishPenalty;
-            result.audioScore = ((audioScore + audioFeature) / 2f) * polishPenalty;
-            result.humorScore = (humorScore * 2f + mechanicsFeature * 0.5f + audioFeature * 0.5f + visualsFeature) / 4f;
-            result.moodScore = ((moodScore * 2f + mechanicsFeature * 0.5f + audioFeature + visualsFeature * 0.5f) / 4f) * polishPenalty;
-            result.overallScore = (result.funScore + result.innovationScore + result.themeScore + result.graphicsScore + result.audioScore + result.humorScore + result.moodScore) / 7f;
+            float finalFunScore = ((funScore * 4f + mechanicsFeature * 2f + audioFeature + visualsFeature) / 8f) * polishPenalty;
+            float finalInnovationScore = (innovationScore * 3f + mechanicsFeature + audioFeature * 0.5f + visualsFeature * 0.5f) / 5f;
+            float finalThemeScore = (themeScore * 3f + mechanicsFeature * 0.5f + audioFeature * 0.75f + visualsFeature * 0.75f) / 5f;
+            float finalGraphicsScore = ((graphicsScore + visualsFeature) / 2f) * polishPenalty;
+            float finalAudioScore = ((audioScore + audioFeature) / 2f) * polishPenalty;
+            float finalHumorScore = (humorScore * 2f + mechanicsFeature * 0.5f + audioFeature * 0.5f + visualsFeature) / 4f;
+            float finalMoodScore = ((moodScore * 2f + mechanicsFeature * 0.5f + audioFeature + visualsFeature * 0.5f) / 4f) * polishPenalty;
+            float overallScore = (finalFunScore + finalInnovationScore + finalThemeScore + finalGraphicsScore + finalAudioScore + finalHumorScore + finalMoodScore) / 7f;
+
+            result.funRank = scoreToRank(finalFunScore);
+            result.innovationRank = scoreToRank(finalInnovationScore);
+            result.themeRank = scoreToRank(finalThemeScore);
+            result.graphicsRank = scoreToRank(finalGraphicsScore);
+            result.audioRank = scoreToRank(finalAudioScore);
+            result.humorRank = scoreToRank(finalHumorScore);
+            result.moodRank = scoreToRank(finalMoodScore);
+            result.overallRank = scoreToRank(overallScore);
 
             return result;
+        }
+
+        private int scoreToRank(float score)
+        {
+            int rank = generateScores().FindLastIndex(x => x < score);
+            if (rank < 0)
+            {
+                return otherContestantCount + 1;
+            }
+            return otherContestantCount - rank;
+        }
+
+        private List<float> generateScores()
+        {
+            List<float> scores = new();
+
+            System.Random rand = new System.Random();
+            const double mean = 0.6f;
+            const double stdDev = 0.15f;
+            for (int i = 0; i < otherContestantCount; ++i)
+            {
+                double u1 = 1.0 - rand.NextDouble();
+                double u2 = 1.0 - rand.NextDouble();
+                double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+                double randNormal = mean + stdDev * randStdNormal;
+                scores.Add(Math.Clamp((float)randNormal, 0f, 0.99f));
+            }
+
+            scores.Sort();
+
+            return scores;
         }
     }
 }
--- LDJAM55/Assets/Scripts/ResultsUI.cs
+++ LDJAM55/Assets/Scripts/ResultsUI.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
 
 public class ResultsUI : MonoBehaviour
 {
@@ -22,13 +19,13 @@ public class ResultsUI : MonoBehaviour
         DataTypes.FinalResult finelResult = backend.ProductState.ComputeFinalResult();
 
         // TODO: Add special cases for 1st and 2nd and 3rd
-        overallText.text = "Overall " + ((int)finelResult.overallScore).ToString() + "th";
-        funText.text = "Fun " + ((int)finelResult.funScore).ToString() + "th";
-        innovationText.text = "Innovation " + ((int)finelResult.innovationScore).ToString() + "th";
-        themeText.text = "Theme " + ((int)finelResult.themeScore).ToString() + "th";
-        graphicsText.text = "Graphics " + ((int)finelResult.graphicsScore).ToString() + "th";
-        audioText.text = "Audio " + ((int)finelResult.audioScore).ToString() + "th";
-        humorText.text = "Humor " + ((int)finelResult.humorScore).ToString() + "th";
-        moodText.text = "Mood " + ((int)finelResult.moodScore).ToString() + "th";
+        overallText.text = "Overall " + ((int)finelResult.overallRank).ToString() + "th";
+        funText.text = "Fun " + ((int)finelResult.funRank).ToString() + "th";
+        innovationText.text = "Innovation " + ((int)finelResult.innovationRank).ToString() + "th";
+        themeText.text = "Theme " + ((int)finelResult.themeRank).ToString() + "th";
+        graphicsText.text = "Graphics " + ((int)finelResult.graphicsRank).ToString() + "th";
+        audioText.text = "Audio " + ((int)finelResult.audioRank).ToString() + "th";
+        humorText.text = "Humor " + ((int)finelResult.humorRank).ToString() + "th";
+        moodText.text = "Mood " + ((int)finelResult.moodRank).ToString() + "th";
     }
 }
